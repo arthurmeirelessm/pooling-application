e0cea4fc836f36e690ef0b4e186f6e412e99eeee
Arthur Henrick Meireles Mendes <ahmmendes@rd.com.br>
2024-08-17 21:10:13 -0300

    feat: add secrets manager to variables of environment

--- src/backend/apis/create_status/repository/dynamo.py
+++ src/backend/apis/create_status/repository/dynamo.py
@@ -14,12 +14,11 @@ class DynamoDBCreateStatusClient:
 
     def __init__(self):
         self.dynamodb = boto3.client("dynamodb")
-        self.table = os.getenv(("DYNAMO_DB_TABLE"))
+        self.secretsmanager = self.initiate_secrets_manager()
 
     def save_requested_data(
         self, id: int
     ) -> Any:
-        print(id)
         try:
             item = {
                 "id": {"S": id},
@@ -27,8 +26,17 @@ class DynamoDBCreateStatusClient:
                 "result": {"S": ""},
             }
 
-            put_requested = self.dynamodb.put_item(TableName=self.table, Item=item)
+            put_requested = self.dynamodb.put_item(TableName=self.secretsmanager, Item=item)
             return put_requested
         except Exception as e:
             return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
         
+    
+    def initiate_secrets_manager(
+        self
+    ) -> Any:
+        secretsmanager = boto3.client("secretsmanager")
+        get_secret_value_response = secretsmanager.get_secret_value(SecretId="pooling-application-secrets")
+        secret = json.loads(get_secret_value_response['SecretString'])
+        dynamo_database = secret.get("DYNAMO-DB-TABLE") 
+        return dynamo_database
